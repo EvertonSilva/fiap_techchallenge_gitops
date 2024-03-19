88fb6d37f21ce817fbf7cf14a76bd38f4284f075
Everton Silva <everton.silva@muralis.com.br>
2024-03-19 09:37:31 -0300

    refactor: sec group mais permissivo

--- eks_cluster/main.tf
+++ eks_cluster/main.tf
@@ -102,9 +102,9 @@ resource "aws_security_group" "cluster_sg" {
   vpc_id      = aws_vpc.postech_fiap_vpc.id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -112,6 +112,7 @@ resource "aws_security_group" "cluster_sg" {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = merge(
